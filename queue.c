1179bcca583d87e2d5e15b55713297a582e427fe
Sven Herzberg <sven@imendio.com>
2008-07-10 02:19:42 +0300

    finish the schedule and queue implementation
    * queue.c: finish the code so it basically does what we want

--- queue.c
+++ queue.c
@@ -26,13 +26,31 @@
 #include "worker.h"
 
 struct _Queue {
-	GList* threads;
-	guint  last_job_id;
+	GList * threads;
+	GList * idle_threads;
+	GQueue* jobs;
+	guint   schedule_idle;
+	guint32 last_job_id;
 };
+
+struct QueueJob {
+	Queue      * queue;
+	Worker     * worker;
+	GThreadFunc  async;
+	gpointer     async_result;
+	GFunc        destroy;
+	gpointer     user_data;
+	guint        id;
+	guint        started : 1;
+	guint        finished : 1;
+};
+
+static void queue_schedule_job (Queue          * queue,
+				struct QueueJob* job);
 
 /*
  * queue_new:
- * n_threads: the number of threads to create
+ * @n_threads: the number of threads to create
  *
  * Create a new workload queue.
  *
@@ -50,12 +68,15 @@ queue_new (gint n_threads)
 
 		if (!error) {
 			queue->threads = g_list_prepend (queue->threads, worker);
+			queue->idle_threads = g_list_prepend (queue->idle_threads, worker);
 		} else {
 			g_printerr ("error creating thread %d (%d of %d)\n",
 				    worker->id, thread + 1, n_threads);
 		}
 	}
 	queue->threads = g_list_reverse (queue->threads);
+
+	queue->jobs = g_queue_new ();
 
 	return queue;
 
@@ -70,31 +91,30 @@ queue_new (gint n_threads)
 void
 queue_free (Queue* queue)
 {
+	if (queue->schedule_idle) {
+		g_warning (G_STRLOC ": FIXME: cancel scheduling thread");
+	}
+
 	while (queue->threads) {
 		Worker* worker = queue->threads->data;
 		worker_shutdown (worker);
 		queue->threads = g_list_delete_link (queue->threads, queue->threads);
 	}
+	g_list_free (queue->idle_threads);
+
+	if (queue->jobs->length) {
+		g_warning (G_STRLOC ": FIXME: cancel unfinished jobs first");
+	}
+	g_queue_free (queue->jobs);
 
 	g_slice_free (Queue, queue);
 }
 
 /* START: QueueJob API */
 
-struct QueueJob {
-	Queue const* queue;
-	GThreadFunc  async;
-	gpointer     async_result;
-	GFunc        destroy;
-	gpointer     user_data;
-	guint        id;
-	guint        started : 1;
-	guint        finished : 1;
-};
-
 static struct QueueJob*
 queue_job_new (guint        id,
-	       Queue const* queue,
+	       Queue      * queue,
 	       GThreadFunc  async,
 	       GFunc        destroy,
 	       gpointer     user_data)
@@ -120,6 +140,74 @@ queue_job_free (struct QueueJob* self)
 
 /* END: QueueJob API */
 
+static gboolean
+job_done (gpointer user_data)
+{
+	struct QueueJob* job = user_data;
+	struct QueueJob* next_job;
+
+	if (job->destroy) {
+		job->destroy (job->async_result,
+			      job->user_data);
+	}
+
+	g_return_val_if_fail (job->queue, FALSE);
+
+	/* return the worker into the list of idle threads */
+	job->queue->idle_threads = g_list_prepend (job->queue->idle_threads,
+						   job->worker);
+
+	next_job = g_queue_pop_head (job->queue->jobs);
+	if (next_job) {
+		queue_schedule_job (job->queue, next_job);
+	}
+
+	queue_job_free (job);
+
+	return FALSE;
+}
+
+static gboolean
+execute_job (gpointer user_data)
+{
+	/* WARNING START: executed in worker thread */
+	struct QueueJob* job = user_data;
+
+	job->async_result = job->async (job->user_data);
+
+	GSource* source = g_idle_source_new ();
+	g_source_set_callback (source,
+			       job_done,
+			       job,
+			       NULL);
+	g_source_attach (source, g_main_context_default ());
+	g_source_unref (source);
+
+	return FALSE; /* execute once */
+	/* WARNING END: executed in worker thread */
+}
+
+static void
+queue_schedule_job (Queue          * queue,
+		    struct QueueJob* job)
+{
+	Worker* worker = queue->idle_threads->data;
+	g_return_if_fail (worker);
+
+	GSource* source = g_idle_source_new ();
+	g_source_set_callback (source,
+			       execute_job,
+			       job,
+			       NULL);
+
+	queue->idle_threads = g_list_delete_link (queue->idle_threads, queue->idle_threads);
+
+	job->worker = worker;
+
+	g_source_attach (source, worker->context);
+	g_source_unref (source);
+}
+
 /*
  * queue_queue:
  * @queue: the #Queue to attach this job request to
@@ -134,7 +222,7 @@ queue_job_free (struct QueueJob* self)
  *
  */
 /* Returns: the id of the job within this queue */
-guint
+guint32
 queue_queue (Queue      * queue,
 	     GThreadFunc  async,
 	     GFunc        destroy,
@@ -143,12 +231,19 @@ queue_queue (Queue      * queue,
 	struct QueueJob* job;
 
 	g_return_if_fail (async != NULL);
+	g_return_if_fail (queue != NULL);
 
 	job = queue_job_new (queue->last_job_id++, queue, async, destroy, user_data);
 
-	g_warning ("FIXME: queue instead of free");
-	queue_job_free (job);
+	if (G_UNLIKELY (queue->idle_threads)) {
+		/* schedule immediately */
+		queue_schedule_job (queue, job);
+	} else {
+		/* queue for scheduling */
+		g_queue_push_tail (queue->jobs,
+				   job);
+	}
 
-	return 0;
+	return job->id;
 }
 
